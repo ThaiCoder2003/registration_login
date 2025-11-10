97e35fe673e5b4daddf155120f1a74c28e96ed74
ThaiCoder2003 <101780284+ThaiCoder2003@users.noreply.github.com>
2025-11-10 10:45:31 +0700

    more config

--- user-registration-backend/src/main.ts
+++ user-registration-backend/src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';                             
 import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: 'http://localhost:5000', // your React app
+    origin: process.env.FRONTEND_URL || '*',// your React app
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   const configService = app.get(ConfigService);
 
-  const port = configService.get<number>('PORT') || 4000;
+  const port = configService.get<number>('PORT') || Number(process.env.PORT) || 4000;
   const nodeEnv = configService.get<string>('NODE_ENV') || 'development';
   const mongoUri = configService.get<string>('MONGO_URI') || 'mongodb://localhost:27017/user_db';
 
